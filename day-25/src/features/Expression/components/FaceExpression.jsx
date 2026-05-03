88eec2ad5240aca6e5cd42f64a5a901f07dc103a
chirag0521 <chiragsarda234@gmail.com>
2026-05-03 15:40:45 +0530

    initialize day-25 project with React and Vite - Create FaceExpression component for facial expression detection - Add utility functions for initializing MediaPipe and detecting expressions

---
+++ day-25/src/features/Expression/components/FaceExpression.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from "react";
+import { detect, init } from "../utils/utils";
+
+
+export default function FaceExpression({ onClick = () => { } }) {
+    const videoRef = useRef(null);
+    const landmarkerRef = useRef(null);
+    const streamRef = useRef(null);
+
+    const [ expression, setExpression ] = useState("Detecting...");
+
+    useEffect(() => {
+        init({ landmarkerRef, videoRef, streamRef });
+
+        return () => {
+            if (landmarkerRef.current) {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                landmarkerRef.current.close();
+            }
+
+            if (videoRef.current?.srcObject) {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                videoRef.current.srcObject
+                    .getTracks()
+                    .forEach((track) => track.stop());
+            }
+        };
+    }, []);
+
+    async function handleClick() {
+        const expression = detect({ landmarkerRef, videoRef, setExpression })
+        console.log(expression)
+        onClick(expression)
+    }
+
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <video
+                ref={videoRef}
+                style={{ width: "400px", borderRadius: "12px" }}
+                playsInline
+            />
+            <h2>{expression}</h2>
+            <button onClick={handleClick} >Detect expression</button>
+        </div>
+    );
+}
