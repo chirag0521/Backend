c89a9b42c921ab3a1345be060eebe5d53b6c95a6
chirag0521 <chiragsarda234@gmail.com>
2026-05-09 08:58:32 +0530

    integrated Redis for token management and caching in authentication

--- day-25/Backend/src/middlewares/auth.middleware.js
+++ day-25/Backend/src/middlewares/auth.middleware.js
@@ -1,18 +1,19 @@
 const blacklistModel = require("../models/blacklist.model")
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const redis = require("../config/cache")
 
 async function authUser(req, res, next) {
     const token = req.cookies.token
+    
     if (!token) {
         return res.status(401).json({
             message: "Token not provided"
         })
     }
 
-    const isTokenBlacklisted = await blacklistModel.findOne({
-        token
-    })
+    const isTokenBlacklisted = await redis.get(token)
+
 
     if(isTokenBlacklisted){
         return res.status(401).json({
