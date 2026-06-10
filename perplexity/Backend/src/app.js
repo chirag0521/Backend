bfb97cfd650188a5406836a2d365f376cd0df957
chirag0521 <chiragsarda234@gmail.com>
2026-06-10 19:15:31 +0530

    added morgan and cors middleware for logging and CORS support

--- perplexity/Backend/src/app.js
+++ perplexity/Backend/src/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
-
+import morgan from "morgan"
+import cors from "cors"
 const app = express()
 
 //Middlewares
 app.use(express.json())
 app.use(cookieParser())
+app.use(morgan("dev"))
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
 
 //Health-check 
 app.get("/", (req, res) => {
